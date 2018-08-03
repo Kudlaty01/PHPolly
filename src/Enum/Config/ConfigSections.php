bcfe47520b8569017018dd0f836d8f29c883ff4d
Paweł Sola <subskrybent.kudlacz01@gmail.com>
2018-08-03 19:23:32 +0200

    Added EnumConfigConfigSections enum class

---
+++ src/Enum/Config/ConfigSections.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Enum\Config;
+
+/**
+ * Class ConfigSections
+ *
+ * @package \Enum\Config
+ */
+class ConfigSections
+{
+	const ROUTES = 'routes';
+	const DB = 'db';
+	const NAVIGATION = 'navigation';
+}
