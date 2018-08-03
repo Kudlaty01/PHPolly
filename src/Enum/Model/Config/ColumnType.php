7c16c008a01e403fbc61183a0ad2b35b17289f79
Paweł Sola <subskrybent.kudlacz01@gmail.com>
2018-08-03 19:23:32 +0200

    Added EnumModelConfigColumnType enum class

---
+++ src/Enum/Model/Config/ColumnType.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Enum\Model\Config;
+
+/**
+ * Class ColumnType
+ *
+ * @package \Enum\Model\Config
+ */
+class ColumnType
+{
+	const INTEGER = 'int';
+	const VARCHAR = 'varchar';
+	const BOOLEAN = 'bool';
+}
