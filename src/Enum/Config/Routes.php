79978de6b0a8a4fc29ef00f55f9b7743545d1240
Paweł Sola <subskrybent.kudlacz01@gmail.com>
2018-08-03 19:23:32 +0200

    Added EnumConfigRoutes enum class

---
+++ src/Enum/Config/Routes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Enum\Config;
+
+/**
+ * Class Routes
+ *
+ * @package \Enum\Config
+ */
+class Routes
+{
+	const ROUTES = 'routes';
+	const ROUTE = 'route';
+	const CONTROLLER = 'controller';
+	const ACTION = 'action';
+	const PARAMETERS = 'parameters';
+	const NAVIGATION = 'navigation';
+}
