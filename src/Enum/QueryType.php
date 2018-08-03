031313975331e229be041ff292fa904dfdd08acf
Paweł Sola <subskrybent.kudlacz01@gmail.com>
2018-08-03 19:23:32 +0200

    Added EnumQueryType enum class

---
+++ src/Enum/QueryType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Enum;
+
+/**
+ * Enum class QueryType for determination of Query type
+ *
+ * @package \Enum
+ */
+class QueryType
+{
+	const SELECT = 1;
+	const INSERT = 2;
+	const UPDATE = 3;
+	const DELETE = 4;
+}
