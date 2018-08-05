16f4c3a72447cfa43d27d00c4e0529ff88af9157
Paweł Sola <subskrybent.kudlacz01@gmail.com>
2018-08-05 22:06:51 +0200

    Changed QueryType enum type from int to string

--- src/Enum/QueryType.php
+++ src/Enum/QueryType.php
@@ -9,8 +9,8 @@ namespace Enum;
  */
 class QueryType
 {
-	const SELECT = 1;
-	const INSERT = 2;
-	const UPDATE = 3;
-	const DELETE = 4;
+	const SELECT = 'select';
+	const INSERT = 'insert';
+	const UPDATE = 'update';
+	const DELETE = 'delete';
 }
