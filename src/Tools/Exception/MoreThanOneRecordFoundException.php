5bd63f771a3a2f0b23819ccb05aa7a160d1d80b6
Paweł Sola <subskrybent.kudlacz01@gmail.com>
2018-08-09 14:31:50 +0200

    define new MoreThanOneRecordFoundException deriving from Exception

---
+++ src/Tools/Exception/MoreThanOneRecordFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tools\Exception;
+
+/**
+ * Class MoreThanOneRecordFoundException
+ *
+ * @package \Tools\Exception
+ */
+class MoreThanOneRecordFoundException extends \Exception
+{
+
+}
